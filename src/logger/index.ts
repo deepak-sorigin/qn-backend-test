d85c9ddbf20a718ebca5f3335a67c0c2a97f3e08
Deepak Tewatia <deepak.tewatia@sorigin.com>
2024-11-14 13:38:12 +0530

    fix: logger fix:

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -7,17 +7,17 @@ const logger = winston.createLogger({
     winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message, ...meta }) => {
       // meta[Symbol.for('splat')] contains the metadata if present
-      let metaString;
-      if (meta) {
-        const metaData = meta[Symbol.for('splat')]
-          ? meta[Symbol.for('splat')][0]
-          : {};
-        if (metaData) {
-          metaString = Object.keys(metaData).length
-            ? ` ${JSON.stringify(metaData)}`
-            : '';
-        }
+      let metaString = '';
+
+      const splatMeta = meta[Symbol.for('splat')] as any[] | undefined;
+
+      if (splatMeta && splatMeta[0]) {
+        const metaData = splatMeta[0];
+        metaString = Object.keys(metaData).length
+          ? ` ${JSON.stringify(metaData)}`
+          : '';
       }
+
       return `${timestamp} [${level}]: ${message} ${metaString} \n`;
     })
   ),
